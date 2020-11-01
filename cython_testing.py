f84c203383f057b664f0265ad831fd24db20d80f
danielnieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2020-11-01 01:55:32 +0000

    Test Cython speed up rate

--- cython_testing.py
+++ cython_testing.py
@@ -13,10 +13,14 @@ engine_move = g.engine_move_decision(1)
 g.board.make_move(engine_move[0],engine_move[1],engine_move[2],engine_move[3])   # Make move
 g.handle_new_successfully_made_move(engine_move)
 """
-
+from statistics import mean
+timedifs = []
 while True:
     py = timeit.timeit(code, setup="from Game import Game", number= 1000)
     cy = timeit.timeit(code, setup="from CythonGame import Game", number= 1000)
+    timedifs.append(py/cy)
+
 
 
     print("C is {} times faster than Python".format(py/cy))
+    print("Average: ", mean(timedifs))
